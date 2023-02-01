d68ebe38a00b64af5cac82f822473679c82a6fc1
c.ismael <hazon.shop@gmail.com>
2023-01-31 23:10:44 -0500

    new changes done in the second class

--- src/Day2GitHub/FirstGitHubClass.java
+++ src/Day2GitHub/FirstGitHubClass.java
@@ -2,6 +2,10 @@ package Day2GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello Batch7.");
+        System.out.println("Hello Batch7🤚.");
+
+
+
+
     }
 }
